6da0f30af8904dc8329992acaf7e835c2aa37d5a
aluno <aluno@ifpe>
2025-04-12 16:02:52 -0300

    envio de arquivos e novas telas

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
+import { Segment } from 'semantic-ui-react';
 import './App.css';
 import Home from './views/home/Home';
 
 function App() {
+
   return (
+
     <div className="App">
-<Home/>
+      
+      <Home />
+
+      <div style={{marginTop: '6%'}}>
+        <Segment vertical color='grey' size='tiny' textAlign='center'>
+          &copy; 2023 - Projeto WEB III - IFPE Jaboatão dos Guararapes
+        </Segment>
+      </div>
+<footer> </footer>
     </div>
   );
 }
