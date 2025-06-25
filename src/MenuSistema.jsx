6ca75d2d563316497e1a48cbc21d2301641fb2ea
thiagosilvabarbosa <contatobarbosathiago@gmail.com>
2025-06-25 19:53:07 -0300

    Aplicando autenticações em formulários

--- src/MenuSistema.jsx
+++ src/MenuSistema.jsx
@@ -1,43 +1,52 @@
-import React from "react";
 import { Link } from "react-router-dom";
 import { Menu } from "semantic-ui-react";
+import { logout } from './views/util/AuthenticationService';
 
-export default function MenuSistema (props) {
 
-   return(
-       <>
-           <Menu inverted>
-              
-               <Menu.Item
-                   content='Home'
-                   active={props.tela === 'home'}
-                   as={Link}
-                   to='/'
-               />
+export default function MenuSistema(props) {
 
-               <Menu.Item
-                   content='Cliente'
-                   active={props.tela === 'cliente'}
-                   as={Link}
-                   to='/list-cliente'
-               />
+    return (
+        <>
+            <Menu inverted>
 
-            
-            <Menu.Item
-                   content='Produto'
-                   active={props.tela === 'produto'}
-                   as={Link}
-                   to='/form-produto'
-               />
+                <Menu.Item
+                    content='Home'
+                    active={props.tela === 'home'}
+                    as={Link}
+                    to='/'
+                />
 
-               <Menu.Item
-                   content='Entregador'
-                   active={props.tela === 'entregador'}
-                   as={Link}
-                   to='/form-entregador'
-               />
+                <Menu.Item
+                    content='Cliente'
+                    active={props.tela === 'cliente'}
+                    as={Link}
+                    to='/list-cliente'
+                />
 
-           </Menu>
-       </>
-   )
+
+                <Menu.Item
+                    content='Produto'
+                    active={props.tela === 'produto'}
+                    as={Link}
+                    to='/form-produto'
+                />
+
+                <Menu.Item
+                    content='Entregador'
+                    active={props.tela === 'entregador'}
+                    as={Link}
+                    to='/form-entregador'
+                />
+
+                <Menu.Item
+                    className='navbar__item--mobile'
+                    onClick={logout}
+                    content='Sair'
+                    as={Link}
+                    to='/'
+                />
+
+            </Menu>
+        </>
+    )
 }
