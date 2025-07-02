cdff1917b643d3d4b9b9f1a2e56eee097a14d873
thiagosilvabarbosa <contatobarbosathiago@gmail.com>
2025-07-02 20:36:53 -0300

    Atualizando list

--- src/views/produto/ListProduto.jsx
+++ src/views/produto/ListProduto.jsx
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { Button, Container, Divider, Header, Icon, Modal, Table } from 'semantic-ui-react';
 import MenuSistema from '../../MenuSistema';
@@ -9,7 +9,14 @@ export default function ListProduto() {
     const [lista, setLista] = useState([]);
     const [openModal, setOpenModal] = useState(false);
     const [idRemover, setIdRemover] = useState();
- 
+    const [menuFiltro, setMenuFiltro] = useState();
+    const [codigo, setCodigo] = useState();
+    const [titulo, setTitulo] = useState();
+    const [idCategoria, setIdCategoria] = useState();
+    const [listaCategoriaProduto, setListaCategoriaProduto] = useState([]);
+
+
+
     useEffect(() => {
         carregarLista();
     }, [])
@@ -17,7 +24,7 @@ export default function ListProduto() {
     function confirmaRemover(id) {
         setOpenModal(true)
         setIdRemover(id)
-    } 
+    }
 
     function carregarLista() {
 
@@ -26,24 +33,82 @@ export default function ListProduto() {
                 setLista(response.data)
             })
     }
+    axios.get("http://localhost:8080/api/categoriaproduto")
+        .then((response) => {
+
+            const dropDownCategorias = [];
+            dropDownCategorias.push({ text: '', value: '' });
+            response.data.map(c => (
+                dropDownCategorias.push({ text: c.descricao, value: c.id })
+            ))
+
+            setListaCategoriaProduto(dropDownCategorias)
+
+        })
+
 
     async function remover() {
 
         await axios.delete('http://localhost:8080/api/produto/' + idRemover)
-        .then((response) => {
-  
-            console.log('Produto removido com sucesso.')
-  
-            axios.get("http://localhost:8080/api/produto")
             .then((response) => {
-                setLista(response.data)
+
+                console.log('Produto removido com sucesso.')
+
+                axios.get("http://localhost:8080/api/produto")
+                    .then((response) => {
+                        setLista(response.data)
+                    })
             })
-        })
-        .catch((error) => {
-            console.log('Erro ao remover um produto.')
-        })
+            .catch((error) => {
+                console.log('Erro ao remover um produto.')
+            })
         setOpenModal(false)
-    } 
+    }
+    function handleMenuFiltro() {
+
+        if (menuFiltro === true) {
+            setMenuFiltro(false);
+        } else {
+            setMenuFiltro(true);
+        }
+    }
+
+    function handleChangeCodigo(value) {
+
+        filtrarProdutos(value, titulo, idCategoria);
+    }
+
+    function handleChangeTitulo(value) {
+
+        filtrarProdutos(codigo, value, idCategoria);
+    }
+
+    function handleChangeCategoriaProduto(value) {
+
+        filtrarProdutos(codigo, titulo, value);
+    }
+    async function filtrarProdutos(codigoParam, tituloParam, idCategoriaParam) {
+
+        let formData = new FormData();
+
+        if (codigoParam !== undefined) {
+            setCodigo(codigoParam)
+            formData.append('codigo', codigoParam);
+        }
+        if (tituloParam !== undefined) {
+            setTitulo(tituloParam)
+            formData.append('titulo', tituloParam);
+        }
+        if (idCategoriaParam !== undefined) {
+            setIdCategoria(idCategoriaParam)
+            formData.append('idCategoria', idCategoriaParam);
+        }
+
+        await axios.post("http://localhost:8080/api/produto/filtrar", formData)
+            .then((response) => {
+                setListaProdutos(response.data)
+            })
+    }
 
     return (
         <div>
@@ -56,6 +121,18 @@ export default function ListProduto() {
                     <Divider />
 
                     <div style={{ marginTop: '4%' }}>
+
+                        <Menu compact>
+                            <Menu.Item
+                                name='menuFiltro'
+                                active={menuFiltro === true}
+                                onClick={() => handleMenuFiltro()}
+                            >
+                                <Icon name='filter' />
+                                Filtrar
+                            </Menu.Item>
+                        </Menu>
+
                         <Button
                             label='Novo'
                             circular
@@ -65,6 +142,43 @@ export default function ListProduto() {
                             as={Link}
                             to='/form-produto'
                         />
+                        {menuFiltro}
+
+                        <Segment>
+                            <Form className="form-filtros">
+
+                                <Form.Input
+                                    icon="search"
+                                    value={codigo}
+                                    onChange={e => handleChangeCodigo(e.target.value)}
+                                    label='Código do Produto'
+                                    placeholder='Filtrar por Código do Produto'
+                                    labelPosition='left'
+                                    width={4}
+                                />
+                                <Form.Group widths='equal'>
+                                    <Form.Input
+                                        icon="search"
+                                        value={titulo}
+                                        onChange={e => handleChangeTitulo(e.target.value)}
+                                        label='Título'
+                                        placeholder='Filtrar por título'
+                                        labelPosition='left'
+                                    />
+                                    <Form.Select
+                                        placeholder='Filtrar por Categoria'
+                                        label='Categoria'
+                                        options={listaCategoriaProduto}
+                                        value={idCategoria}
+                                        onChange={(e, { value }) => {
+                                            handleChangeCategoriaProduto(value)
+                                        }}
+                                    />
+
+                                </Form.Group>
+                            </Form>
+                        </Segment>
+
                         <br /><br /><br />
 
                         <Table color='orange' sortable celled>
@@ -123,24 +237,24 @@ export default function ListProduto() {
             </div>
 
             <Modal
-               basic
-               onClose={() => setOpenModal(false)}
-               onOpen={() => setOpenModal(true)}
-               open={openModal}
-         >
-               <Header icon>
-                   <Icon name='trash' />
-                   <div style={{marginTop: '5%'}}> Tem certeza que deseja remover esse registro? </div>
-               </Header>
-               <Modal.Actions>
-                   <Button basic color='red' inverted onClick={() => setOpenModal(false)}>
-                       <Icon name='remove' /> Não
-                   </Button>
-                   <Button color='green' inverted onClick={() => remover()}>
-                       <Icon name='checkmark' /> Sim
-                   </Button>
-               </Modal.Actions>
-         </Modal>
+                basic
+                onClose={() => setOpenModal(false)}
+                onOpen={() => setOpenModal(true)}
+                open={openModal}
+            >
+                <Header icon>
+                    <Icon name='trash' />
+                    <div style={{ marginTop: '5%' }}> Tem certeza que deseja remover esse registro? </div>
+                </Header>
+                <Modal.Actions>
+                    <Button basic color='red' inverted onClick={() => setOpenModal(false)}>
+                        <Icon name='remove' /> Não
+                    </Button>
+                    <Button color='green' inverted onClick={() => remover()}>
+                        <Icon name='checkmark' /> Sim
+                    </Button>
+                </Modal.Actions>
+            </Modal>
 
         </div>
     )
